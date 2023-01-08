304014b37f3a6b7d6320fb0c7a20bd017f977696
Md-Swapan <swapan@excelitai.com>
2023-01-08 11:03:37 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>bpp shop </h1>
-      <p>New project start</p>
-      <h1>bpp shop new frontend</h1>
-
-      <p>welcome to new project</p>
-      <h6>SHuvo+swapan</h6>
+      <h1>hello bppshops</h1>
     </div>
   );
 }
