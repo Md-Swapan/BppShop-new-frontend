149115e9b5b71bb3f551882d5df02291418135df
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-01-08 18:57:34 +0600

    home page update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="m-5">
       <button class="btn w-64 rounded-full">Button</button>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
-
-      <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+      <p>
+        In publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.
+      </p>
     </div>
   );
 };
