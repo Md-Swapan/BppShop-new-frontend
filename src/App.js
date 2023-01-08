b4ce8d34907cb22d2403dd67651a84f35b7bcbb9
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-01-08 11:04:23 +0600

    lorem text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
       <p>welcome to new project</p>
       <h6>SHuvo+swapan</h6>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quas beatae quibusdam mollitia. Excepturi sequi ipsum delectus repellat cumque reprehenderit, deleniti aperiam magnam dicta adipisci ratione architecto, sed libero molestiae.</p>
     </div>
   );
 }
